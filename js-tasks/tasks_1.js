1de452e49624aa22e4ec4d96e67cb822c8c79f07
OleksSavchenko <98595558+OleksSavchenko@users.noreply.github.com>
2023-07-21 09:36:59 +0000

    Formatted js tasks folder

---
+++ js-tasks/tasks_1.js
@@ -0,0 +1,35 @@
+//Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+
+
+
+
+//Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+
+
+
+
+//Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+
+
+
+
+//Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+
+
+
+
+//Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+
+
+
+
+//Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+
+
+
