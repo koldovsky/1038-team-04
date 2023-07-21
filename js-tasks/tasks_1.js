e9ad2760799cd41cf878b5eef57cdccf0241a593
Olha <64465872+OlhaPivniuk@users.noreply.github.com>
2023-07-21 17:55:06 +0300

    Update tasks_1.js

--- js-tasks/tasks_1.js
+++ js-tasks/tasks_1.js
@@ -5,20 +5,7 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 
 //Make negative https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 function makeNegative(num) {
-  if (num > 0) {
-    return num * (-1);
-  } else {
-    return num ;
-  }
-}
-//////////////////////////
-function makeNegative(num) {
   return num > 0 ? -num : num;
-}
-//////////////////////////
-function makeNegative(num) {
-  if (num < 0) return num;
-  return -num;
 }
 
 //Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
@@ -28,20 +15,7 @@ function move(position, roll) {
 
 //Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 function greet(name, owner) {
-      if (name === owner) {
-    return 'Hello boss';
-  } else {
-    return 'Hello guest';
-  }
-}
-////////////////////////////
-function greet(name, owner) {
   return name === owner ? "Hello boss" : "Hello guest";
-}
-////////////////////////////
-function greet(name, owner) {
-  if (name === owner) return "Hello boss";
-  return "Hello guest";
 }
 
 //Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
@@ -51,22 +25,5 @@ function litres(time) {
 
 //Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 function lovefunc(flower1, flower2){
-    if (flower1 % 2 == flower2 % 2) {
-    return false;
-  } else {
-    return true;
-  }
-}
-////////////////////////
-function lovefunc(flower1, flower2){
   return (flower1 + flower2) % 2 === 1;
-}
-///////////////////////
-function lovefunc(flower1, flower2) {
-  if (
-    (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
-    (flower1 % 2 !== 0 && flower2 % 2 === 0)
-  )
-    return true;
-  return false;
 }
