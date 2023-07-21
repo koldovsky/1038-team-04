2cb03340520d3b5ac09db82b6ce8190b17a51502
OleksSavchenko <98595558+OleksSavchenko@users.noreply.github.com>
2023-07-21 17:12:53 +0000

    Created files for js tasks 2

---
+++ js-tasks/tasks_2.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+//Поглиблені задачки (робити за бажанням):
+//https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
+
+//https://www.codewars.com/kata/beginner-lost-without-a-map
+
+//https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
---
+++ js-tasks/tasks_2_additional.js
@@ -0,0 +1,22 @@
+//Додаткові задачки по JS, щоб формувати навички, якщо маєте час і бажання 
+//Double Integer    https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+
+//Century From Year    https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+
+//Multyply two numbers    https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+
+//Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+//Cockroach speed    https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
+
+//Angle of triangle    https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
+
+//Twice as old    https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+
+//Return n-th even number    https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+
+//What's the real floor     https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+
+//Clock    https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+
+//Is n divisible by x and y    https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
