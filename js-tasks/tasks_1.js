2045a425e6ec609f86af4e4ffabaec91fe1c0ddb
romanmyko <123646984+romanmyko@users.noreply.github.com>
2023-07-21 13:06:44 +0300

    Update tasks_1.js myko :)

--- js-tasks/tasks_1.js
+++ js-tasks/tasks_1.js
@@ -1,34 +1,57 @@
 //Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
-
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+}
 
 
 
 
 //Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
-
+function makeNegative(num) {
+  if (num > 0) {
+    return num * (-1);
+  } else {
+    return num ;
+  }
+}
 
 
 
 
 //Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
-
+function move (position, roll) {
+  return position + roll * 2 
+}
 
 
 
 
 //Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
-
-
-
-
+function greet (name, owner) {
+      if (name === owner) {
+    return 'Hello boss';
+  } else {
+    return 'Hello guest';
+  }
+}
 
 //Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
-
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
 
 
 
 
 //Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+function lovefunc(flower1, flower2){
+  // moment of truth
+    if (flower1 % 2 == flower2 % 2) {
+    return false;
+  } else {
+    return true ;
+  }
+}
 
 
 
