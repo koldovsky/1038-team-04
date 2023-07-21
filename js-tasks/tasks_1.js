872182080b523e1a9bfbf9e59f517f020d7d5de1
OleksSavchenko <98595558+OleksSavchenko@users.noreply.github.com>
2023-07-21 13:42:45 +0300

    Added alternative solutions to tasks_1.js

--- js-tasks/tasks_1.js
+++ js-tasks/tasks_1.js
@@ -3,10 +3,7 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals
 }
 
-
-
-
-//Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+//Make negative https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 function makeNegative(num) {
   if (num > 0) {
     return num * (-1);
@@ -14,17 +11,15 @@ function makeNegative(num) {
     return num ;
   }
 }
-
-
-
+//////////////////////////
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
 
 //Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 function move (position, roll) {
   return position + roll * 2 
 }
-
-
-
 
 //Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 function greet (name, owner) {
@@ -34,14 +29,15 @@ function greet (name, owner) {
     return 'Hello guest';
   }
 }
+////////////////////////////
+function greet (name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
+}
 
 //Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 function litres(time) {
   return Math.floor(time * 0.5);
 }
-
-
-
 
 //Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 function lovefunc(flower1, flower2){
@@ -52,7 +48,10 @@ function lovefunc(flower1, flower2){
     return true ;
   }
 }
-
+////////////////////////
+function lovefunc(flower1, flower2){
+  return (flower1 + flower2) % 2 === 1 ? true : false;
+}
 
 
 
