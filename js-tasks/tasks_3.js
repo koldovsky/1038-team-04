c4bf57f6ff8b4556637986db6d492d8d940c626b
OleksSavchenko <98595558+OleksSavchenko@users.noreply.github.com>
2023-07-24 18:15:26 +0000

    Created tasks_3.js

---
+++ js-tasks/tasks_3.js
@@ -0,0 +1,16 @@
+//https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
+
+// https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
+
+
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+
+
+// опційна поглиблена задачка про замикання (дивіться відео від мене спочатку https://youtu.be/XJdzZgzLc08)
+// https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+
+
+// опційна поглиблена задачка про наслідування 
+// https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+
