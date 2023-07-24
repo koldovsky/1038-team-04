740c4095ce9f8c8542eef450da862d9b7293cab5
OleksSavchenko <98595558+OleksSavchenko@users.noreply.github.com>
2023-07-24 11:07:33 +0000

    Updated tasks_2 files

--- js-tasks/tasks_2.js
+++ js-tasks/tasks_2.js
@@ -1,49 +1,46 @@
 //https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
-function stringToArray(string){
-  return string.split(' ')
+function stringToArray(string) {
+    return string.split(' ');
 }
 
 //https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 function DNAtoRNA(dna) {
-    return dna.replace(/T/g,'U');
-    }
+    return dna.replace(/T/g, 'U');
+}
 
 //https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
-var min = function(list){
+var min = function (list) {
     return Math.min(...list);
 }
 
-var max = function(list){
+var max = function (list) {
     return Math.max(...list);
 }
 
 //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 function min(arr, toReturn) {
-    if (toReturn === 'value'){
-        return Math.min(...arr);
-    }
-    return arr.indexOf(Math.min(...arr))
-  }
+    return toReturn === 'value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr));
+}
 //Поглиблені задачки (робити за бажанням):
 //https://www.codewars.com/kata/a-wolf-in-sheeps-clothing/train/javascript
 function warnTheSheep(queue) {
     return (queue.indexOf('wolf') === queue.length - 1) ? "Pls go away and stop eating my sheep" :
-        "Oi! Sheep number " + (queue.length - queue.indexOf('wolf') - 1) + "! You are about to be eaten by a wolf!"
+        "Oi! Sheep number " + (queue.length - queue.indexOf('wolf') - 1) + "! You are about to be eaten by a wolf!";
 }
 //https://www.codewars.com/kata/beginner-lost-without-a-map
 function maps(x) {
-    let result = []
+    let result = [];
     for (let i of x) {
         result.push(i * 2);
     }
-    return result
+    return result;
 }
 
 //https://www.codewars.com/kata/find-the-first-non-consecutive-number/train/javascript
 function firstNonConsecutive(arr) {
-    let res = []
+    let res = [];
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] + 1 !== arr[i + 1]) { res.push(arr[i + 1]) }
+        if (arr[i] + 1 !== arr[i + 1]) res.push(arr[i + 1])
     }
     return (res[0] === undefined) ? null : res[0];
 }
--- js-tasks/tasks_2_additional.js
+++ js-tasks/tasks_2_additional.js
@@ -7,22 +7,22 @@ function doubleInteger(i) {
 
 //Century From Year    https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
 function century(year) {
-  return Math.ceil(year/100);
+  return Math.ceil(year / 100);
 }
 
 //Multyply two numbers    https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
 
-const multiply = (x,y) => x * y  
+const multiply = (x, y) => x * y;
 
 //Make negative    https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 function makeNegative(num) {
-  return -Math.abs(num)
+  return -Math.abs(num);
 }
 
 //Cockroach speed    https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
 function cockroachSpeed(s) {
-    return Math.floor( s * 100000 / 3600 )
-  }
+  return Math.floor(s * 100000 / 3600);
+}
 
 //Angle of triangle    https://www.codewars.com/kata/5a023c426975981341000014/train/javascript
 function otherAngle(a, b) {
@@ -32,25 +32,24 @@ function otherAngle(a, b) {
 //Twice as old    https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
 
 function twiceAsOld(dadYearsOld, sonYearsOld) {
- return Math.abs(dadYearsOld - sonYearsOld * 2)
+  return Math.abs(dadYearsOld - sonYearsOld * 2);
 }
 
 //Return n-th even number    https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
-function nthEven(n){
-  return  n * 2 - 2
+function nthEven(n) {
+  return n * 2 - 2;
 }
 
 //What's the real floor     https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
 function getRealFloor(n) {
-  if (n >= 13 ) {return n - 2};
-  if (n <= 0 ) { return n };  
-  return n - 1
-} 
+  return n >= 13 ? n - 2 : n <= 0 ? n : n - 1;
+}
 //Clock    https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
-function past(h, m, s){
-  return (h * 3600 + m * 60 + s) * 1000
+function past(h, m, s) {
+  return (h * 3600 + m * 60 + s) * 1000;
 }
 
 //Is n divisible by x and y    https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
-return !(n/x  === Math.round( n /x ) && n/y === Math.round(n /y ));
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0;
 }
