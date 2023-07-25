f4eb95bfdb7ce7330f125cc4f82710fa7de979dd
Roman Myko <123646984+romanmyko@users.noreply.github.com>
2023-07-25 22:40:10 +0300

    Update tasks_3.js start

--- js-tasks/tasks_3.js
+++ js-tasks/tasks_3.js
@@ -1,11 +1,28 @@
 //https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
-
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args)
+  }
+}
 
 // https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
-
+function circleCircumference(circle) {
+  const radius = circle.radius;
+  const res = 2 * Math.PI * radius;
+    return res;                 
+ }
 
 // https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
-
+function giveMeFive(obj){
+  const res = [] 
+  for (var key in obj){
+    if ( key.length == 5) {  
+      res.push(key) ;}
+    if ( obj[key].length == 5 ) { 
+      res.push(obj[key]); } 
+   }
+  return res
+}
 
 // опційна поглиблена задачка про замикання (дивіться відео від мене спочатку https://youtu.be/XJdzZgzLc08)
 // https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
