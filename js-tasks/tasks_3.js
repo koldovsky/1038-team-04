3ac26c6d10545a14524bef22bc70260d6da0dfc0
OleksSavchenko <98595558+OleksSavchenko@users.noreply.github.com>
2023-07-26 13:51:38 +0300

    Added last task to tasks_3.js

--- js-tasks/tasks_3.js
+++ js-tasks/tasks_3.js
@@ -37,4 +37,29 @@ function buildFun(n) {
 
 // опційна поглиблена задачка про наслідування 
 // https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, 'shark', status);
+  }
+}
 
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, 'cat', status)
+  }
+  
+  introduce() {
+    return super.introduce()+'  Meow meow!'
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, 'dog', status)
+    this.master = master
+  }
+  
+  greetMaster() {
+    return `Hello ${this.master}`
+  }
+}
