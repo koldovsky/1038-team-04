d5a40dc333783cadab207ec84cef02640d504acf
Roman Myko <123646984+romanmyko@users.noreply.github.com>
2023-07-26 09:31:39 +0300

    Update tasks_3.js 4 task

--- js-tasks/tasks_3.js
+++ js-tasks/tasks_3.js
@@ -26,7 +26,14 @@ function giveMeFive(obj){
 
 // опційна поглиблена задачка про замикання (дивіться відео від мене спочатку https://youtu.be/XJdzZgzLc08)
 // https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
-
+function buildFun(n) {
+  var res = [];
+  for (var i = 0; i < n; i++) {
+    (function(current) {res.push(function() {return current; });
+  })(i);
+  }
+  return res;
+}
 
 // опційна поглиблена задачка про наслідування 
 // https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
